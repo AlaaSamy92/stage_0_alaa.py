f0184b3163e9d346b07240e1dc62d07cb93787f2
AlaaSamy92 <69093973+AlaaSamy92@users.noreply.github.com>
2020-08-04 07:16:11 +0200

    Add files via upload

---
+++ stage_0_Alaa.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import pandas as pd
+data=pd.read_excel("Python_alaa.xlsx")
+my_data=data.iloc[0]
+print(my_data)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
